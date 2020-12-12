df484621a9a8e24f6297d578f95f4bc5e55c210f
Ashley Dixon <ashleydixon2012@gmail.com>
2020-12-11 22:02:29 -0700

    Def init_browser and scrape_info

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -6,3 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def init_browser():
     executable_path = {'exectuable_path': "/usr/local/bin/chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
+
+def scrape_info():
+    browser = init_browser()
+    
