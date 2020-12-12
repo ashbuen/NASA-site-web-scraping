1f833d569de8845cdae52c9a814baf69e5f1477e
Ashley Dixon <ashleydixon2012@gmail.com>
2020-12-11 21:56:04 -0700

    Added beginning code for scrape file

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,8 @@
+# Import dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+
+def init_browser():
+    executable_path = {'exectuable_path': "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
