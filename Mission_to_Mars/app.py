a2293653bace61cb6c91306880edb054d1cea178
Ashley Dixon <ashleydixon2012@gmail.com>
2020-12-12 01:47:22 -0700

    Updated notebook

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,5 +1,5 @@
 # Import dependencies
-from flask import Flask, render_template, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
@@ -12,16 +12,18 @@ mongo = PyMongo(app, uri = "mongodb://localhost:27017/mars_db")
 # Landing page setup
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    mars_db = mongo.db.collection.find_one()
+    return render_template("index.html", mars=mars_db)
 
 # Scrape the scrape_mars file and when done return to landing page
 @app.route("/scrape")
 def scraper():
-    mars = mongo.db.mars_info
-    mars_info = scrape_mars.scrape()
-    mars.update({}, mars_info, upsert=True)
-    return redirect("/", code=382)
+    # mars_data = mongo.db.mars
+    mars_info = scrape_mars.scrape_info()
+    mongo.db.collection.update({}, mars_info, upsert=True)
+
+# Redirect to homepage
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -3,50 +3,101 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import time
 
 def init_browser():
-    executable_path = {'exectuable_path': "/usr/local/bin/chromedriver"}
+    executable_path = {'exectuable_path': "ChromeDriverManager().install()"}
     return Browser("chrome", **executable_path, headless=False)
 
 def scrape_info():
     browser = init_browser()
     mars = {}
+    time.sleep(1)
 
-# Url
-    url = 'https://mars.nasa.gov/news/'
-    browser.visit(url)
-
-# HTML object
-    html = browser.html
-
-# Parser the html
-    soup = bs(html, "html.parser")
-    print(soup.prettify())
     
-# Scrape latest title
-    latest_title = soup.select('div.content_title')[1]
-    latest_title
+# # Url
+#     url = 'https://mars.nasa.gov/news/'
+#     browser.visit(url)
 
-# Scrape paragraph of latest title
-    latest_paragraph = soup.find_all('div', 'article_teaser_body')[0]
-    latest_paragraph
+# # HTML object
+#     html = browser.html
 
-# Quit Mars scraping
-    browser.quit()
+# # Parser the html
+#     soup = bs(html, "html.parser")
+#     print(soup.prettify())
+    
+# # Scrape latest title
+#     latest_title = soup.select('div.content_title')[1]
+#     latest_title
 
-# Splinter setup for JPL
-    executable_path = {'executable_path' : "/usr/local/bin/chromedriver"}
-    browser = Browser('chrome', **executable_path, headless = False)
+# # Scrape paragraph of latest title
+#     latest_paragraph = soup.find_all('div', 'article_teaser_body')[0]
+#     latest_paragraph
 
-# JPL Url
-    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    browser.visit(url2)
-    html = browser.html
-    soup = bs(html, 'html.parser')
-    print(soup.prettify())
+# # JPL Url
+#     url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+#     browser.visit(url2)
+#     html = browser.html
+#     soup = bs(html, 'html.parser')
+#     print(soup.prettify())
 
-# JPL featured image
-    featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/mediumsize/PIA09178_ip.jpg'
+# # JPL featured image
+#     featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/mediumsize/PIA09178_ip.jpg'
 
-# Quit JPL browser
-    browser.quit()
+# # Mars Facts pandas
+#     url3 = 'https://space-facts.com/mars/'
+#     browser.visit(url3)
+
+
+# # Read Mars table
+#     Mars_facts_table = pd.read_html(url3)
+#     Mars_facts_table
+
+#     type(Mars_facts_table)
+
+# # Store table into a dataframe
+#     df = Mars_facts_table[0]
+#     df
+
+# # Renamed header columns
+#     renamed = df.rename(columns={0 : " ", 1 : "Mars"})
+#     renamed
+
+# # Removed index column
+#     No_index = renamed.rename(index={0 :" ",
+#                     1:" ",
+#                     2:" ",
+#                     3:" ",
+#                     4:" ",
+#                     5:" ",
+#                     6:" ",
+#                     7:" ",
+#                     8:" "})
+#     No_index
+
+# # DF table to HTML table
+#     html_table = No_index.to_html()
+#     html_table
+
+# # Remove newlines
+#     html_table.replace('\n', '')
+
+# # Store HTML table as a file
+#     No_index.to_html('table.html')
+
+# # Url
+#     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+#     browser.visit(url)
+
+# # Dictionary for Mars images
+#     hemisphere_image_urls = [
+#         {"title" : "Valles Marineris Hemisphere", "img_url" : "https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg" },
+#         {"title" : "Cereberus Hemisphere", "img_url" : "https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg" },
+#         {"title" : "Schiaparelli Hemisphere", "img_url" : "https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg" },
+#         {"title" : "Syrtis Major Hemisphere", "img_url" : "https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg" }
+#     ]
+#     hemisphere_image_urls
+
+#     browser.quit()
+    
+#     return mars
