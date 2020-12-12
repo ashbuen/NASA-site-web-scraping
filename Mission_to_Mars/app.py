2797f1130a7e6c1942a16a6380b903f687a180c5
Ashley Dixon <ashleydixon2012@gmail.com>
2020-12-11 23:19:52 -0700

    Setup scrape route

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -15,9 +15,13 @@ mongo = PyMongo(app)
 def index():
 
 
+# Scrape the scrape_mars file and when done return to landing page
 @app.route("/scrape")
 def scraper():
-
+    mars = mongo.db.mars_info
+    mars_info = scrape_mars.scrape()
+    mars.update({}, mars_info, upsert=True)
+    return redirect("/", code=382)
 
 if __name__ = "__main__":
     app.run(debug=True)
